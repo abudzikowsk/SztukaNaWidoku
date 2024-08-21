f138824ac8698d8755a3dec1a232f6a95c2be69a
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2024-08-21 22:25:39 +0200

    Update angular to v18

--- SztukaNaWidoku/ClientApp/src/app/app.component.ts
+++ SztukaNaWidoku/ClientApp/src/app/app.component.ts
@@ -4,12 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {EventBoxComponent} from "./event-box/event-box.component";
 import {ExhibitionModel} from "./models/exhibition.model";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, EventBoxComponent, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, EventBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
