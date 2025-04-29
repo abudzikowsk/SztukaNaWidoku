a81949cf814dd4c153b51e49335bcf6fdcd907c8
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2025-04-29 23:41:11 +0200

    add migrations with removed decription

---
+++ SztukaNaWidoku/Migrations/20250429213323_remove description field.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SztukaNaWidoku.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedescriptionfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Exhibitions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Exhibitions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
